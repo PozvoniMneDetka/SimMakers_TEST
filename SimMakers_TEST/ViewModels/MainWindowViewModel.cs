30d4a1ad6abf795940663ff4a72b21ccfc6e2bf7
Nagishom <40797226+PozvoniMneDetka@users.noreply.github.com>
2022-03-09 17:31:08 +0700

    Тест работы модели

---
+++ SimMakers_TEST/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SimMakers_TEST.ViewModels.Base;
+
+namespace SimMakers_TEST.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        #region Заголовок
+        private string _title = "Тестовое задание SimMakers";
+        /// <summary>Заголовок</summary>
+        public string Title
+        {
+            get => _title;
+            set => Set(ref _title, value);
+        }
+        #endregion
+    }
+}
