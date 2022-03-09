6fb3705eb5717d0d5c477603f42c605c7defbad5
Nagishom <40797226+PozvoniMneDetka@users.noreply.github.com>
2022-03-09 16:24:21 +0700

    Класс модели-представления

---
+++ SimMakers_TEST/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace SimMakers_TEST.ViewModels.Base
+{
+    internal abstract class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string _propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(_propertyName));
+        }
+
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string _propertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(_propertyName);
+            return true;
+        }
+    }
+}
